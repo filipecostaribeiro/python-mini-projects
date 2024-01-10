fb50cfe77bc45b3c02a5acb17854610912728ccf
filipecostaribeiro <119543434+filipecostaribeiro@users.noreply.github.com>
2024-01-10 13:09:22 -0300

    commit

--- pysnake/test.py
+++ pysnake/test.py
@@ -1,24 +1,13 @@
-a = r"""
-           /^\/^\
-         _|__|  O|
-\/     /~     \_/ \
- \____|__________/  \
-        \_______      \
-                `\     \                 \
-                  |     |                  \
-                 /      /                    \
-                /     /                       \\
-              /      /                         \ \
-             /     /                            \  \
-           /     /             _----_            \   \
-          /     /           _-~      ~-_         |   |
-         (      (        _-~    _--_    ~-_     _/   |
-          \      ~-____-~    _-~    ~-_    ~-_-~    /
-            ~-_           _-~          ~-_       _-~
-               ~--______-~                ~-___-~
+import pandas as pd
+import os
+from tabulate import tabulate
+# Read CSV file into a DataFrame
+current_directory = os.path.abspath(os.path.dirname(__file__))
+file_path = os.path.join(current_directory, "scores.csv")
+df = pd.read_csv(file_path, index_col=0)
 
+# Display the DataFrame using tabulate
+table = tabulate(df, headers='keys', tablefmt='grid')
 
-
-"""
-
-print(a)
+# Print the table
+print(table)
